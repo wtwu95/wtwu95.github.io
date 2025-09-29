2cd7632ddcc473f6d61512a390c2d642dada6e53
wtwu95 <154416851+wtwu95@users.noreply.github.com>
2025-09-29 18:26:39 +0800

    Revert "Align author maps layout and sizing"

--- assets/js/map-reorder.js
+++ assets/js/map-reorder.js
@@ -41,96 +41,6 @@
     }
 
     var mediaQuery = window.matchMedia ? window.matchMedia(BREAKPOINT_MAX) : null;
-    var scheduleSharedHeightUpdate = function () {};
-
-    initSharedMapHeight();
-
-    function initSharedMapHeight() {
-      if (!mapsContainer) {
-        return;
-      }
-
-      var visitorsMap = mapsContainer.querySelector('.author__map--visitors');
-      var locationMap = mapsContainer.querySelector('.author__map--location');
-      if (!visitorsMap || !locationMap) {
-        return;
-      }
-
-      var requestFrame =
-        typeof window.requestAnimationFrame === 'function'
-          ? window.requestAnimationFrame.bind(window)
-          : function (callback) {
-              return window.setTimeout(callback, 16);
-            };
-      var rafId = null;
-      var ensureIntervalId = null;
-
-      function setSharedHeight(height) {
-        if (!mapsContainer) {
-          return;
-        }
-
-        if (height > 0) {
-          mapsContainer.style.setProperty('--author-map-shared-height', height + 'px');
-        }
-      }
-
-      function runUpdate() {
-        rafId = null;
-        if (!visitorsMap) {
-          return;
-        }
-
-        var rect = visitorsMap.getBoundingClientRect();
-        if (rect.height > 0) {
-          setSharedHeight(rect.height);
-        }
-      }
-
-      function scheduleUpdate() {
-        if (rafId !== null) {
-          return;
-        }
-
-        rafId = requestFrame(runUpdate);
-      }
-
-      scheduleSharedHeightUpdate = scheduleUpdate;
-
-      if (typeof ResizeObserver === 'function') {
-        var resizeObserver = new ResizeObserver(scheduleUpdate);
-        resizeObserver.observe(visitorsMap);
-      } else {
-        window.addEventListener('resize', scheduleUpdate);
-      }
-
-      if (typeof MutationObserver === 'function') {
-        var mutationObserver = new MutationObserver(scheduleUpdate);
-        mutationObserver.observe(visitorsMap, {
-          childList: true,
-          subtree: true,
-          attributes: true
-        });
-      }
-
-      var attempts = 0;
-      ensureIntervalId = window.setInterval(function () {
-        attempts += 1;
-        var rect = visitorsMap.getBoundingClientRect();
-        if (rect.height > 0 || attempts > 20) {
-          if (rect.height > 0) {
-            setSharedHeight(rect.height);
-          }
-          if (ensureIntervalId) {
-            window.clearInterval(ensureIntervalId);
-            ensureIntervalId = null;
-          }
-        }
-      }, 200);
-
-      window.addEventListener('load', scheduleUpdate);
-      scheduleUpdate();
-    }
 
     function shouldMoveToContent() {
       return mediaQuery ? mediaQuery.matches : window.innerWidth <= 924;
@@ -141,7 +51,6 @@
         pageInner.appendChild(mapSidebar);
       }
       mapSidebar.classList.add('author__map-sidebar--inline');
-      scheduleSharedHeightUpdate();
     }
 
     function moveToSidebar() {
@@ -149,7 +58,6 @@
         placeholder.parentNode.insertBefore(mapSidebar, placeholder);
       }
       mapSidebar.classList.remove('author__map-sidebar--inline');
-      scheduleSharedHeightUpdate();
     }
 
     function updatePlacement() {
